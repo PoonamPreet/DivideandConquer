27f43fcdb04d9106b9d1f09aae6134579609f5e0
Poonam <anmolpreetsmailbox@gmail.com>
2022-02-20 19:52:48 +0530

    Divide and conquer merge sort

---
+++ src/DNC.java
@@ -0,0 +1,70 @@
+
+public class DNC {
+
+	static void mergesort(int []a1,int start, int end)
+	{
+		if(start<end)
+		{
+			int mid =(start+end)/2;
+			mergesort(a1, start, mid);
+			mergesort(a1, mid+1, end);
+			merge(a1,start,mid,end);
+		}
+	}
+	
+	static void merge(int a1[],int start, int mid,int end)
+	{
+		int i,j,k;
+		int lsize=mid-start+1;//left array size 
+		int rsize=end-mid;//right array size
+		int l1[]=new int[mid+1];
+		int r1[]=new int[end-mid];
+		for(i=0;i<lsize;i++)
+		{
+			l1[i]=a1[start+i];
+		}
+		for(j=0;j<rsize;j++)
+		{
+			r1[j]=a1[mid+1+j];
+		}
+		i=0;
+		j=0;
+		k=start;
+		while(i<lsize && j<rsize)
+		{
+			if(l1[i]<=r1[j])
+			{
+				a1[k]=l1[i];
+				i++;
+			}
+			else
+			{
+				a1[k]=r1[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<lsize)
+		{
+			a1[k]=l1[i];
+			i++;
+			k++;
+		}
+		while(j<rsize)
+		{
+			a1[k]=r1[j];
+			j++;
+			k++;
+		}
+	}
+	public static void main(String[] args) 
+	{
+		int[] a1=new int[] {3,5,7,1,4,9};
+		mergesort(a1, 0, a1.length-1);
+		for(int i=0;i<a1.length;i++)
+		{
+			System.out.print(a1[i]+" ");
+		}
+	}
+
+}
